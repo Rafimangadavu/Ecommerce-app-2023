df2d1019e3ce06bfadec06164c8b74d77da003ab
RafiMangadavu <rafimangadavu2025@gmail.com>
2023-06-03 21:18:27 +0530

    cart itme remove completed

--- client/src/context/cart.js
+++ client/src/context/cart.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 
 const CartContext = createContext();
@@ -6,6 +6,10 @@ const CartContext = createContext();
 const CartProvider = ({children}) =>{
     const [cart, setCart] = useState([])
 
+    useEffect(() => {
+        let existingCartItem = localStorage.getItem('cart')
+        if(existingCartItem)setCart(JSON.parse(existingCartItem))
+    },[])
 
     return(
         <CartContext.Provider value={[cart, setCart]}>
