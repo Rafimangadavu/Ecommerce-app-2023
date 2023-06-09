2e676fb5f6a2b06314c67261f39d50a7e8a129df
Mohammed Rafi Mangadavu <74808630+Rafimangadavu@users.noreply.github.com>
2023-06-09 16:39:41 +0530

    Update server.js
    add file url path

--- server.js
+++ server.js
@@ -8,12 +8,17 @@ import categoryRoutes from './routes/categoryRoutes.js';
 import productRoutes from './routes/productRoutes.js';
 import cors from 'cors';
 import path from 'path';
+import { fileURLToPath} from 'url';
 
 //configure env
 dotenv.config();
 
 //database config
 connectDB();
+
+//esmodule fix
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 //rest object
 const app = express();
